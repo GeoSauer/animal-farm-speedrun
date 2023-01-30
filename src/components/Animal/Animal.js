f929873d87129027394f9d53ae95b976d83d275b
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-30 10:32:31 -0700

    That's all folks

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img src={`/animals/${props.type}.svg`} />
+      <span className="name">{props.name}</span>
+      <span>{props.says}</span>
+    </div>
+  );
 }
